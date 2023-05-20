f67b07cc1e6d5324fbf62d37bcd4bac9e513e78a
imcaptaingit <imcaptaingit@gmail.com>
2023-05-19 19:48:45 -0700

    Homewrok completed

---
+++ src/java_week_1_writing_hw_sarvat/Programme_1_BioData.java
@@ -0,0 +1,23 @@
+package java_week_1_writing_hw_sarvat;
+/**
+ * 1. Write a program that prints your BIO Data.
+ */
+public class Programme_1_BioData {
+
+    public static void main(String[] args) {
+        System.out.println("                    BIO DATA                    ");
+        System.out.println("Name                                : Sarvat Shaikh");
+        System.out.println("Mobile                              : 07404933243");
+        System.out.println("Email                               : sarvatshaiikh@gmail.com");
+        System.out.println("Gender                              : Male");
+        System.out.println("Date of Birth                       : 09.08.1995");
+        System.out.println("Marital Status                      : Married");
+        System.out.println("Languages Known                     : English, Hindi and Gujarati");
+        System.out.println("Qualification                       : Software Tester");
+        System.out.println("Experience                          : Agile Tester with 5 years of experience");
+        System.out.println("Address                             : 123 Abc Street, London AB1 2CD");
+        System.out.println("Place                               : London");
+        System.out.println("Date                                : 19.05.2023");
+        System.out.println("Signature                           : SShaikh");
+    }
+}
---
+++ src/java_week_1_writing_hw_sarvat/Programme_2_PrintFace.java
@@ -0,0 +1,13 @@
+package java_week_1_writing_hw_sarvat;
+
+public class Programme_2_PrintFace {
+
+    public static void main(String[] args) {
+
+        System.out.println(" +\"\"\"\"\"+");
+        System.out.println("[| o o |]");
+        System.out.println(" |  ^  | ");
+        System.out.println(" | '_' | ");
+        System.out.println(" +_____+");
+    }
+}
